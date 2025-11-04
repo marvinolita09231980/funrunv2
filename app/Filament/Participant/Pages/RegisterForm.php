182aae0f2127149222cf3a30f8c4524bed245901
marvinolita09231980 <marvinolitagithub@gmail.com>
2025-11-04 15:00:06 +0800

    update3

--- app/Filament/Participant/Pages/RegisterForm.php
+++ app/Filament/Participant/Pages/RegisterForm.php
@@ -5,18 +5,31 @@ namespace App\Filament\Participant\Pages;
 use Filament\Pages\Page;
 use App\Models\Participant;
 use App\Models\Subcategory;
+use Filament\Support\RawJs;
 use Filament\Schemas\Schema;
 use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\DB;
+use Filament\Forms\Components\Radio;
+use Illuminate\Support\Facades\Auth;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+
+use Filament\Schemas\Components\Form;
+use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
 use Filament\Schemas\Components\Section;
+use Filament\Forms\Components\DatePicker;
+use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Contracts\HasSchemas;
 use Illuminate\Support\Facades\Exceptions;
 use Filament\Forms\Components\MarkdownEditor;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 use Illuminate\Validation\ValidationException;
+use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use App\Filament\Resources\Participants\Schemas\ParticipantForm;
-use Illuminate\Support\Facades\DB;
 
 class RegisterForm extends Page implements HasSchemas
 {
@@ -30,21 +43,197 @@ class RegisterForm extends Page implements HasSchemas
     {
         return __('Fun Run 2025');
     }
-    public function mount(): void
-    {
-        $this->form->fill([
-             'waiver' => false
-          
-        ]);
-    }
+     
     
+
+     public function mount(): void
+     {
+         $this->form->fill([
+              'waiver' => false
+         ]);
+     }
+
+
     public function form(Schema $schema): Schema
     {
        
-        return ParticipantForm::configure($schema)
-                ->extraAttributes(['class' => ''])
-                ->statePath('data');
+        return $schema
+           ->components([
+            Fieldset::make('Personal Information')
+                ->schema([
+                    TextInput::make('firstName')
+                            ->required()
+                            ->columnSpan(2)
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set, $get,$component) {
+                                    $exists = self::checkAndFillParticipant($get, $set);
+                                    
+                            }),
+                    TextInput::make('lastName')
+                            ->required()
+                            ->columnSpan(2)
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set, $get,$component) {
+                                    $exists =self::checkAndFillParticipant($get, $set);
+                                    
+                            }),
+                    TextInput::make('middleInitial')
+                            ->maxLength(1),
+                    DatePicker::make('birthDate')
+                            ->columnSpan(2)
+                            ->required()
+                            ->reactive()
+                            ->afterStateUpdated(function ($state, callable $set, $get,$component) {
+                                   $exists = self::checkAndFillParticipant($get, $set);
+                                  
+                            }),
+
+                    Select::make('gender')
+                            ->required()
+                            ->options([
+                                'male' =>     'Male',
+                                'female' => 'Female',
+                                'non-binary' => 'Non-binary',
+                                'transgender' => 'Transgender',
+                                'genderqueer' => 'Genderqueer',
+                                'agender' => 'Agender',
+                                'prefer not to say' => 'Prefer not to say'
+                            ])
+                            ->searchable()
+                            ->columnSpan(3),
+                    TextInput::make('address')
+                            ->columnSpan(5)
+                            ->required(),
+
+                    TextInput::make('contactNumber')
+                            ->minLength(11)
+                            ->mask(RawJs::make(<<<'JS'
+                                    '9999 999 9999'
+                                JS))
+                            ->stripCharacters(' ')
+                            ->columnSpan(2),
+                
+                ])
+                ->columns(5),
+                Fieldset::make('Funrun Information')
+                    ->schema([
+                        Select::make('categoryDescription')
+                            ->label('Category')
+                            ->required()
+                            ->options([
+                                'OPEN CATEGORY' => 'OPEN CATEGORY',
+                            ])
+                            ->searchable()
+                            ->columnSpanFull() 
+                            ->hint(function ($get) {
+                                $year = now()->year;
+                                $category = $get('categoryDescription');
+                              
+
+                                // 🔹 Only run when both values are selected
+                                if (! $category) {
+                                    return null;
+                                }
+
+                                $cat = DB::table('subcategories as s')
+                                    ->leftJoin('participants as p', function ($join) use ($year) {
+                                        $join->on('p.categoryDescription', '=', 's.categoryDescription')
+                                            ->where('p.year', '=', $year);
+                                    })
+                                    ->select(
+                                        's.nop',
+                                        's.categoryDescription',
+                                        DB::raw('COUNT(p.id) as registered_count')
+                                    )
+                                    ->where('s.categoryDescription', $category)
+                                    ->groupBy('s.nop', 's.categoryDescription')
+                                    ->first();
+
+                                if ($cat) {
+                                    return $cat->registered_count >= $cat->nop
+                                        ? '⚠️ Participant slots already full.'
+                                        : null;
+                                }
+
+                                return null;
+                            })
+                            ->hintColor('danger')
+                            ->reactive(),
+                        TextInput::make('subDescription')
+                            ->label('Description')
+                            ->required()
+                            ->columnSpanFull(),
+                            
+                        Select::make('shirtSize')
+                            ->options([
+                                'xs' =>     'XS',
+                                'small' => 'Small',
+                                'medium' => 'Medium',
+                                'large' => 'Large',
+                                'xl' => 'XL',
+                                '2xl' => '2XL',
+                                '3xl' => '3XL',
+                                '4xl' => '4XL',
+                                '5xl' => '5XL'
+                            ])
+                            ->searchable()
+                            ->required(),
+                            
+                        Radio::make('distanceCategory')
+                            ->required()
+                            ->options([
+                                    '3km' =>    '3KM',
+                                    '5km' =>    '5KM',
+                                    '10km' =>   '10KM',
+                                ])
+                            ->inline()
+                            ->reactive(),
+                    ]),
+                    Toggle::make('waiver')
+                    ->label('Waiver?')
+                    ->default(false)
+                    ->inline(false)
+                    ->hidden(),        
+             ])
+            ->statePath('data');
     }
+
+    private static function checkAndFillParticipant(callable $get, callable $set): bool
+    {
+        $firstName = $get('firstName');
+        $lastName = $get('lastName');
+        $birthDate = $get('birthDate');
+        $yearNow =  date('Y');
+       
+        
+        
+        if ($firstName && $lastName && $birthDate) {
+            $participant = Participant::where('year', $yearNow)
+                ->where('firstName', $firstName)
+                ->where('lastName', $lastName)
+                ->whereDate('birthDate', $birthDate)
+                ->first();
+
+            if ($participant) {
+              
+                $set('middleInitial', $participant->middleInitial);
+                $set('gender', $participant->gender);
+                $set('address', $participant->address);
+                $set('contactNumber', $participant->contactNumber);
+                $set('categoryDescription', $participant->categoryDescription);
+                $set('subDescription', $participant->subDescription);
+                $set('shirtSize', $participant->shirtSize);
+                $set('distanceCategory', $participant->distanceCategory);
+                $set('referenceNumber', $participant->referenceNumber);
+                return true;
+            } 
+        }
+        return false;
+    }
+    
+   
+
+   
 
     public function create()
     {
@@ -75,11 +264,9 @@ class RegisterForm extends Page implements HasSchemas
                             DB::raw("(SELECT COUNT(*) 
                                       FROM participants 
                                       WHERE participants.categoryDescription = subcategories.categoryDescription 
-                                      AND participants.subDescription = subcategories.subDescription 
                                       AND participants.year = '2025') AS registered"),
                         ])
                         ->where('categoryDescription',$this->data['categoryDescription'])
-                        ->where('subDescription',$this->data['subDescription'])
                         ->first();
 
           
